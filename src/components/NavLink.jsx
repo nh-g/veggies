6109527f60b3278fdd933e45deaa39cdf11faab2
nh-g <giangngohong@gmail.com>
2021-09-08 01:01:23 +0200

    fix nav link to capitalized

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -2,15 +2,17 @@ import logo from "../assets/images/logo/veggie50.svg";
 import { Link } from "react-router-dom";
 
 export default function NavLink({ item: {title, path, isImage } }) {
+  const capitalizedTitle = title.charAt(0).toUpperCase() + title.slice(1);
+  
   return (
     <li className={`nav-item ${title}-item`}>
       <Link to={path}>
         <a>
-        {isImage ? (
-          <img className="logo" src={logo} alt="Logo" />
-        ) : (
-          title.toUpperCase()
-        )}
+          {isImage ? (
+            <img className="logo" src={logo} alt="Logo" />
+          ) : (
+            capitalizedTitle
+          )}
         </a>
       </Link>
     </li>
