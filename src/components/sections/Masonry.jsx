bff54060e922125fbe8048079442f8423b8accc8
nh-g <giangngohong@gmail.com>
2021-09-08 11:47:50 +0200

    refactor: make Masonry component to be reusable

---
+++ src/components/sections/Masonry.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+import ImageLoader from "../ImageLoader";
+
+export default function Masonry({ item }) {
+  const { urlParameter, title, description, imgFilePath } = item;
+  return (
+    <div className="masonry">
+      <Link to={`/categories/${urlParameter}`}>
+        <div className="left-content">
+          <ImageLoader filePath={imgFilePath} />
+        </div>
+      </Link>
+      <div className="right-content">
+        <h2>{title.toUpperCase()}</h2>
+        <p>{description}</p>
+        <Link to={`/categories/${urlParameter}`}>
+          <span className="cta">View {title.toUpperCase()}</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,13 @@ import { getCategories } from '../data/categoryMasonry.js';
 
 import MappingList from '../components/atoms/MappingList.jsx';
 import Hero from '../components/sections/Hero.jsx';
-import CategoryMasonry from '../components/sections/CategoryMasonry.jsx';
+import Masonry from '../components/sections/Masonry.jsx';
 export default function HomePage() {
     return (
       <div className="home">
         <Hero />
         <div className="spacer intro"/>
-        <MappingList getData={getCategories()} Component={CategoryMasonry} />
+        <MappingList getData={getCategories()} Component={Masonry} />
         <div className="spacer intro"/>
 
       </div>
