e972325169c092f48fb08e5b4a95bf7170ac2492
nh-g <giangngohong@gmail.com>
2021-09-10 15:54:12 +0200

    remove comment out

--- src/components/sections/Ingredients.jsx
+++ src/components/sections/Ingredients.jsx
@@ -1,18 +1,13 @@
 export default function Ingredients({ item }) {
   return (
     <>
-      {/* <ul> */}
-        {item.map((ingredient) => {
+    {item.map((ingredient) => {
           return (
-            // <li key={ingredient}>
               <div key={ingredient} className="pill ingredients">
-                {/* <p>{ingredient}</p> */}
                 {ingredient}
               </div>
-            // </li>
           );
         })}
-      {/* </ul> */}
     </>
   );
 }
