1da568648236dcc08966302d61fa966799ffa9e6
nh-g <giangngohong@gmail.com>
2021-09-10 12:08:07 +0200

    remove pagerender, instead add route to single pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,36 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
-import PageRenderer from "./Page-renderer";
 
 import NavigationBar from "./components/NavigationBar";
 import Footer from "./components/Footer";
 
+import HomePage from './pages/homepage';
 import ProductPage from "./pages/productpage";
-import DishPage from './pages/dishpage';
+import CategoryPage from "./pages/categoryPage";
+import ContactPage from './pages/contactpage';
 
 import './css/style.css';
 export default function App() {
   return (
-    <Router>
-      <div className="App">
+    <div className="App">
+      <Router>
         <NavigationBar />
         <div className="main">
           <Switch>
-            <Route exact path="/:page" component={PageRenderer} />
             <Route exact path="/" render={() => <Redirect to="/home" />} />
+            <Route exact path="/home" component={HomePage} />
+            <Route exact path="/contact" component={ContactPage} />
+            <Route exact path="/:category" component={CategoryPage} />
             <Route
               exact
               path="/:category/:product_id"
               component={ProductPage}
             />
-            <Route component={() => 404} />
           </Switch>
         </div>
         <Footer />
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
