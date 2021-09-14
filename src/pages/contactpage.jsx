1e00565b1ee8bcba7bda84916cc25d0201f56028
nh-g <giangngohong@gmail.com>
2021-09-14 15:13:37 +0200

    fix cover photo to be responsive on media queries

--- src/pages/contactpage.jsx
+++ src/pages/contactpage.jsx
@@ -16,7 +16,7 @@ export default function ContactPage() {
           </p>
         </div>
 
-        <h2>Book a table with us</h2>
+        <h2>Book a table with us or just say Hi!</h2>
         <ContactForm />
 
         <div className="address">
