3766e0851b78dadf2f7ffd74933462a84c89fbb9
nh-g <giangngohong@gmail.com>
2021-09-17 05:00:43 +0200

    fix styles

--- src/components/sections/NutritionFacts.jsx
+++ src/components/sections/NutritionFacts.jsx
@@ -1,44 +1,46 @@
 export default function NutritionFacts({ product }) {
   const calories = product.nutrition_facts[0];
-  const otherData = product.nutrition_facts.slice(
-    1,
-    product.nutrition_facts.length
-  );
+  const otherData = product.nutrition_facts.slice(1,product.nutrition_facts.length);
+  const NutritionPercentages = otherData.map((item) => {
+    return (
+      <tr key={item.id}>
+        <td>
+          <h4>{item.label}</h4>
+        </td>
+        <td>
+          <p>{item.value}%</p>
+        </td>
+      </tr>
+    );
+  });
 
   return (
     <section className="nutrition_facts">
-      <h2>Nutrition Facts</h2>
-      <div className="separator15" />
-      <div className="calories-count">
-        <h1>{calories.value}</h1>
-        <h5>calories</h5>
-      </div>
-      <div className="separator5" />
-      <p className="daily_value">% Daily Value*</p>
+      <header>
+        <h2>Nutrition Facts</h2>
+        <h5>Serving Size 1/2 cup (about 82g) </h5>
+        <h5>Serving Per Portion 3</h5>
+        <div className="separator15" />
+        <div className="calories-count">
+          <h1>{calories.value}</h1>
+          <h5>calories</h5>
+          <h4 className="daily_value">% Daily Value*</h4>
+        </div>
+      </header>
+
+      <div className="separator8" />
 
       <table>
-        <tbody>
-          {otherData.map((item) => {
-            return (
-              <tr key={item.id}>
-                <td>
-                  <h4>{item.label}</h4>
-                </td>
-                <td>
-                  <p>{item.value}%</p>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
+        <tbody>{NutritionPercentages}</tbody>
       </table>
+
       <div className="separator15" />
-      <br />
-      <span>
+      
+      <h5>
         *The % Daily Value tells you how much a nutrient in a serving food
         contributes to a daily die. 2000 calories a day is used for general
         nutrition advice.
-      </span>
+      </h5>
     </section>
   );
 }
