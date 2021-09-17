e177ca6ab823bf010c27376d8f69458e3c73ebea
nh-g <giangngohong@gmail.com>
2021-09-17 05:00:20 +0200

    remove function inside jsx html

--- src/components/sections/Ingredients.jsx
+++ src/components/sections/Ingredients.jsx
@@ -1,13 +1,10 @@
 export default function Ingredients({ item }) {
-  return (
-    <>
-    {item.map((ingredient) => {
-          return (
-              <div key={ingredient} className="pill ingredients">
-                {ingredient}
-              </div>
-          );
-        })}
-    </>
-  );
+  const Ingredients = item.map((ingredient) => {
+    return (
+      <div key={ingredient} className="pill ingredients">
+        {ingredient}
+      </div>
+    );
+  });
+  return <>{Ingredients}</>;
 }
