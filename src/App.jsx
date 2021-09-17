5f659b20341aca4a38696e223d9e0ac3e69392d9
nh-g <giangngohong@gmail.com>
2021-09-17 05:43:11 +0200

    remove exact; /home redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 import NavigationBar from "./shared/NavigationBar";
 import Footer from "./shared/Footer";
@@ -14,16 +9,17 @@ import CategoryPage from "./pages/CategoryPages/Category";
 import ContactPage from './pages/ContactPage/Contact';
 
 import './css/style.css';
+
 export default function App() {
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter>
         <NavigationBar />
+
         <div className="main">
           <Switch>
-            <Route exact path="/" render={() => <Redirect to="/home" />} />
-            <Route exact path="/home" component={HomePage} />
-            <Route exact path="/contact" component={ContactPage} />
+            <Route exact path="/" component={HomePage} />
+            <Route path="/contact" component={ContactPage} />
             <Route exact path="/:category" component={CategoryPage} />
             <Route
               exact
@@ -32,8 +28,9 @@ export default function App() {
             />
           </Switch>
         </div>
+        
         <Footer />
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
